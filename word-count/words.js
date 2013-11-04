d950a4bfe17304567cf16d0de57b16fd088e3287
bjohn465 <bjohn465@gmail.com>
2013-11-03 22:05:57 -0700

    Adding "word count" solution that passes all tests

--- word-count/words.js
+++ word-count/words.js
@@ -4,11 +4,22 @@ var hasProperty = function( obj, property ) {
 	return Object.prototype.hasOwnProperty.call( obj, property );
 };
 
+var getCurrentCount = function( word, count ) {
+	return hasProperty( count, word ) ? count[ word ] : 0;
+};
+
+var normalizeWord = function( word ) {
+	return word.toLowerCase();
+};
+
 var Words = function( input ) {
 	var count = {};
-	input.split( " " ).forEach( function( word ) {
-		var currentCount = hasProperty( count, word ) ? count[ word ] : 0;
-		count[ word ] = currentCount + 1;
+	input.split( /[^\w]+/ ).forEach( function( word ) {
+		word = normalizeWord( word );
+		if ( word.length === 0 ) {
+			return;
+		}
+		count[ word ] = getCurrentCount( word, count ) + 1;
 	});
 
 	this.count = count;
