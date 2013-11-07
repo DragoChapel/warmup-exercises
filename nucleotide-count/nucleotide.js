3ae7a122802e95e004f561a87d13744a4a3925a1
bjohn465 <bjohn465@gmail.com>
2013-11-07 07:38:03 -0700

    Counting actual nucleotides
    This isn't passing all tests and the code needs to be cleaned up
    (see TODO comments)

--- nucleotide-count/nucleotide.js
+++ nucleotide-count/nucleotide.js
@@ -2,19 +2,46 @@
 
 var validNucleotides = [ "A", "C", "G", "T" ];
 
-var defaultCounts = (function( validNucleotides ) {
+var hasProperty = function( obj, property ) {
+	return Object.prototype.hasOwnProperty.call( obj, property );
+};
+
+var getCurrentCount = function( nucleotide, counts ) {
+	return hasProperty( counts, nucleotide ) ? counts[ nucleotide ] : 0;
+};
+
+var countNucleotides = function( nucleotidesArray, defaultCounts ) {
+	var counts = defaultCounts;
+	nucleotidesArray.forEach( function( nucleotide ) {
+		counts[ nucleotide ] = getCurrentCount( nucleotide, counts ) + 1;
+	});
+	return counts;
+};
+
+var nucleotideSequenceToArray = function( nucleotideSequence ) {
+	return nucleotideSequence.split( "" );
+};
+
+var getDefaultCounts = function( validNucleotides ) {
+
+	// TODO: This is too similar to `countNucleotides`
 	var counts = {};
 	validNucleotides.forEach( function( nucleotide ) {
 		counts[ nucleotide ] = 0;
 	});
 	return counts;
-})( validNucleotides );
+};
 
 var DNA = function( nucleotideSequence ) {
-	this.nucleotideCounts = defaultCounts;
+	this.nucleotideCounts = countNucleotides(
+		nucleotideSequenceToArray( nucleotideSequence ),
+		getDefaultCounts( validNucleotides )
+	);
 };
 
 DNA.prototype.count = function( nucleotide ) {
+
+	// TODO: Handle "U" somwhere
 	return this.nucleotideCounts[ nucleotide ];
 };
 
